9bb7cbfceafb6224d7f7f702faff60036b74498e
Kevin <kkxandeer@gmail.com>
2019-01-23 16:54:04 +0800

    feat: Set css indent to 2

---
+++ css-custom.el
@@ -0,0 +1,23 @@
+;;; css-custom.el --- js configuration
+
+;; Author: Kevin Du
+;; Maintainer: Kevin Du
+;; Version: 0.1
+;; Homepage: https://github.com/xandeer/purcell-custom
+
+;;; Commentary:
+
+;; commentary
+
+;;; Code:
+
+(add-hook 'css-mode
+          (setq css-indent-offset 2))
+
+(provide 'css-custom)
+
+;; Local Variables:
+;; coding: utf-8
+;; no-byte-compile: t
+;; End:
+;;; css-custom.el ends here
