0cd9dbb0e9e703ccb1112245e072b4a04079986a
Kevin <kkxandeer@gmail.com>
2019-04-16 14:33:01 +0800

    feat: Add a function to move headline to the top of its section

--- org-custom.el
+++ org-custom.el
@@ -96,6 +96,37 @@
      (setq org-map-continue-from (outline-previous-heading)))
    "/DONE" 'tree))
 
+(defun org-custom-to-top ()
+  "Move the current org headline to the top of its section."
+
+  (interactive)
+  ;; check if we are at the top level
+
+  (let ((lvl (org-current-level)))
+    (cond
+     ;; above all headlines so nothing to do
+     ((not lvl)
+      (message "No headline to move"))
+     ((= lvl 1)
+      ;; if at top level move current tree to go above first headline
+      (org-cut-subtree)
+      (beginning-of-buffer)
+      ;; test if point is now at the frst headline and if not then move
+      ;; to the first headline
+      (unless (looking-at-p "*")
+        (org-next-visible-heading 1))
+      (org-paste-subtree))
+     ((> lvl 1)
+      ;; if not at top level then get position of headline level above
+      ;; current section and refile to that position.
+      (let* ((org-reverse-note-order t)
+             (pos (save-excursion
+                    (outline-up-heading 1)
+                    (point)))
+             (filename (buffer-file-name))
+             (rfloc (list nil filename nil pos)))
+        (org-refile nil nil rfloc))))))
+
 (provide 'org-custom)
 
 ;; Local Variables:
